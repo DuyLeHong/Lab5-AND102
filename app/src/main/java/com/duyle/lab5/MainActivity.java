1d543ea5d06ea671704761eb6406e0d1e53b5ecd
Duy Le <lehongduy.bk@gmail.com>
2024-11-14 10:12:18 +0700

    update fragment

--- app/src/main/java/com/duyle/lab5/MainActivity.java
+++ app/src/main/java/com/duyle/lab5/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         drawerLayout = findViewById(R.id.drawerlayout);
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout,toolbar, R.string.open, R.string.close);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar, R.string.open, R.string.close);
 
         drawerToggle.setDrawerIndicatorEnabled(true);
         drawerLayout.addDrawerListener(drawerToggle);
@@ -49,19 +50,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+        Fragment fragment;
+
         if (item.getItemId() == R.id.item_home) {
             Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
             toolbar.setTitle("Trang chu");
+            fragment = FragmentHome.newInstance();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, fragment).commit();
         } else if (item.getItemId() == R.id.item_bookmark) {
             Toast.makeText(getApplicationContext(), "Bookmark", Toast.LENGTH_SHORT).show();
             toolbar.setTitle("Bookmark");
+
+            fragment = FragmentBookmark.newInstance();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, fragment).commit();
         } else if (item.getItemId() == R.id.item_settings) {
             Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
             toolbar.setTitle("Settings");
+
+            fragment = FragmentSettings.newInstance();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, fragment).commit();
         } else if (item.getItemId() == R.id.item_logout) {
             Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
             toolbar.setTitle("Logout");
         }
+
 
         drawerLayout.closeDrawers();
         return true;
